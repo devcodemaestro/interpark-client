8c4ddc7cebe3903aa09acf0418b85f5e53c460d6
devcodemaestro <dev.codemaestro@gmail.com>
2023-10-24 16:01:11 +0900

    feat : header 영역 기초 완료

--- src/components/Footer.js
+++
@@ -1,4 +0,0 @@
-export default function Footer() {
-  // javascripts 코드 자리
-  return <footer class="footer">하단</footer>;
-}
--- src/components/Header.js
+++
@@ -1,4 +0,0 @@
-export default function Headers() {
-  // javascripts 코드 자리
-  return <header class="headers">상단</header>;
-}
