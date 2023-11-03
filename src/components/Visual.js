6c650136ce1940e93062b9d57dec9817ce09117c
devcodemaestro <dev.codemaestro@gmail.com>
2023-11-03 15:51:38 +0900

    feat : visual 영역 json 연동완료

--- src/components/Visual.js
+++ src/components/Visual.js
@@ -1,14 +1,77 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
+
 import "swiper/css";
+
 import "../styles/visual.css";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Visual() {
   // js 코드 자리
-  // jsx에 작성된 html 태그를 React에서 참조
+  // JSX 에 작성된 html 태그를 React 에서 참조
   // 1. swiper 슬라이드 태그를 참조한다.
   const swiperRef = useRef();
+
+  // 외부 데이터 연동 ( fetch 활용)
+
+  const fetchGetData = () => {
+    fetch("visual.json")
+      .then((res) => res.json())
+      .then((result) => {
+        // console.log(result);
+        // 자료를 출력하자.
+        makeVisualSlide(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  // visual 슬라이드 내용 채우는 기능
+  // 리액트용 변수 : 컴포넌트에 출력할 JSX
+  //       일반변수 말고  리액트용 변수를 state 라고 합니다.
+  let [visualHtml, setVisualHtml] = useState([]);
+
+  const makeVisualSlide = (_data) => {
+    const visualRes = _data;
+    // console.log(visualRes);
+    // for(초기값; 조건식; 증감식) {
+    //   할일
+    // };
+    let visualArray = [];
+    for (let i = 0; i < visualRes.total; i++) {
+      // console.log("visual_" + (i + 1));
+      visualArray[i] = visualRes["visual_" + (i + 1)];
+    }
+    console.log(visualArray);
+    setVisualHtml(visualArray);
+
+    // // 배열 자료(visualArray) 를 뜯어서 컴포넌트 담기
+    // let slideArray = [];
+    // for (let i = 0; i < visualRes.total; i++) {
+    //   slideArray[i] = <SwiperSlide></SwiperSlide>;
+    // }
+    // console.log(slideArray);
+  };
+
+  // 화면이 보이면
+  // 컴포넌트가 보이면
+  // 컴포넌트가 랜더링 되면
+  //     데이터 호출 및 배치
+  //  주로 하는 작업
+  //   1. 네트워크 연동 외부 데이터 불러들임
+  //   2. html 을 제어할 때
+  //   3. 윈도우 (window) 를 제어할때
+  //   4. window.addEventLisener 작성할때
+  //   5. window.removeEventListner 작성할떄
+  //   6. 컴포넌트가 삭제 될때
+  useEffect(() => {
+    // 랜더링 될때
+    //  visual.json 데이터 불러들이기 기능실행
+    fetchGetData();
+    return () => {
+      // 삭제될때 (Clean Up 함수)
+    };
+  }, []);
 
   return (
     <section className="visual">
@@ -27,55 +90,20 @@ function Visual() {
           }}
           className="visual-slide"
         >
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img
-                  src={process.env.PUBLIC_URL + "images/v1.png"}
-                  alt="a.html"
-                />
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/v2.jpg`}
-                  alt="a.html"
-                />
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img src="/images/v3.jpg" alt="a.html" />
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img src="images/v4.jpg" alt="a.html" />
-              </a>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img src="images/v5.jpg" alt="a.html" />
-              </a>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <div className="visual-slide-item">
-              <a href="a.html">
-                <img src="images/v6.png" alt="a.html" />
-              </a>
-            </div>
-          </SwiperSlide>
+          {visualHtml.map((item, idx) => {
+            return (
+              <SwiperSlide key={idx}>
+                <div className="visual-slide-item">
+                  <a href={item.url}>
+                    <img
+                      src={process.env.PUBLIC_URL + item.file}
+                      alt={item.file}
+                    />
+                  </a>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
 
         <button
@@ -84,6 +112,7 @@ function Visual() {
             swiperRef.current.slidePrev();
           }}
         ></button>
+
         <button
           className="visual-slide-next"
           onClick={() => {
